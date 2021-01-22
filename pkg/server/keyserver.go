85a21ee095e2f6f1fef9a40b09f1c07fdb5b64bd
Charlie Jonas <charlie@charliejonas.co.uk>
2021-01-22 22:22:31 +0000

    Only use HTML error pages if client can accept them

--- pkg/server/keyserver.go
+++ pkg/server/keyserver.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/CHTJonas/httkey-server/assets"
 	"github.com/CHTJonas/httkey-server/pkg/utils"
@@ -27,23 +28,23 @@ func (k *Keyserver) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	reqPath := r.URL.Path
 	host, _, err := net.SplitHostPort(r.Host)
 	if err != nil {
-		errorHTML(w, "500 Internal Server Error", err.Error(), http.StatusInternalServerError)
+		respondWithError(w, r, "500 Internal Server Error", err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	hash, err := utils.SplitURLToHash(host, reqPath)
 	if err != nil {
-		errorHTML(w, "400 Bad Request", err.Error(), http.StatusBadRequest)
+		respondWithError(w, r, "400 Bad Request", err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	path := filepath.Join(k.StaticPath, hash)
 	_, err = os.Stat(path)
 	if os.IsNotExist(err) {
-		errorHTML(w, "404 Not Found", "The URL does not exist.", http.StatusNotFound)
+		respondWithError(w, r, "404 Not Found", "The URL does not exist.", http.StatusNotFound)
 		return
 	} else if err != nil {
-		errorHTML(w, "500 Internal Server Error", err.Error(), http.StatusInternalServerError)
+		respondWithError(w, r, "500 Internal Server Error", err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -52,7 +53,7 @@ func (k *Keyserver) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r2 := k.setRequestPathToHash(hash, r)
 		http.FileServer(http.Dir(k.StaticPath)).ServeHTTP(w, r2)
 	default:
-		errorHTML(w, "405 Method Not Allowed", "The request method is inappropriate.", http.StatusMethodNotAllowed)
+		respondWithError(w, r, "405 Method Not Allowed", "The request method is inappropriate.", http.StatusMethodNotAllowed)
 	}
 }
 
@@ -65,13 +66,30 @@ func (k *Keyserver) setRequestPathToHash(hash string, r *http.Request) *http.Req
 	return r2
 }
 
-func errorHTML(w http.ResponseWriter, title, message string, statusCode int) {
-	template := assets.MustAsset("assets/error.html.mustache")
-	context := map[string]string{"title": title, "message": message}
-	body, err := mustache.Render(string(template), context)
-	if err != nil {
-		http.Error(w, message, statusCode)
+func respondWithError(w http.ResponseWriter, r *http.Request, title, message string, statusCode int) {
+	body := fmt.Sprintf("%s\n%s\n", title, message)
+	if clientAcceptsHTML(r) {
+		template := assets.MustAsset("assets/error.html.mustache")
+		context := map[string]string{"title": title, "message": message}
+		html, err := mustache.Render(string(template), context)
+		if err != nil {
+			http.Error(w, message, statusCode)
+			return
+		}
+		body = html
 	}
 	w.WriteHeader(statusCode)
 	fmt.Fprintf(w, body)
+}
+
+func clientAcceptsHTML(r *http.Request) bool {
+	h := r.Header.Get("Accept")
+	for _, s := range strings.Split(h, ",") {
+		for _, t := range strings.Split(s, ";") {
+			if t == "text/html" {
+				return true
+			}
+		}
+	}
+	return false
 }
