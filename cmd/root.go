5cd11a2da6cd4552e7850c99d07fad089d961a08
Charlie Jonas <charlie@charliejonas.co.uk>
2024-11-05 19:43:50 +0000

    Fix very minor typo

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ var rootCmd = &cobra.Command{
 	Use:   "httkey",
 	Short: "HTTP server that serves files based on the hash of the URL",
 	Long: "httkey is a web server that serves static files in response to HTTP requests. " +
-		"The static files are all stored in a single directory with filenames that are a hash of their URL." +
+		"The static files are all stored in a single directory with filenames that are a hash of their URL. " +
 		"The hash used is a 128-bit MurmurHash3 of the request path, keyed on the HTTP Host.",
 }
 
